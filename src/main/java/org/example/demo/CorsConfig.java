d2ed3d197b3c61f9624565034a6eae0476ba72f5
Sree7032 <141319594+Sree7032@users.noreply.github.com>
2025-09-21 14:22:56 +0530

    Push

--- src/main/java/org/example/demo/CorsConfig.java
+++ src/main/java/org/example/demo/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         // ✅ Allow both local dev and deployed Vercel frontend
                         .allowedOrigins(
-                                "https://freact-k2fr.vercel.app"
+                                "https://freact-two.vercel.app/"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
